caa0fe61cf0cad2386b6fb0992bf2700e46a630c
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2022-03-21 11:52:30 +0100

    v0.1
    Add onclick sounds playback.
    Edit CSS.

---
+++ index.js
@@ -0,0 +1,13 @@
+// Array of sounds
+const sounds = ["https://pokemoncries.com/cries-old/1.mp3", "https://pokemoncries.com/cries-old/4.mp3", "https://pokemoncries.com/cries-old/7.mp3"]
+
+// Get all elements with class pokemon
+const pokemons = document.querySelectorAll(".pokemon");
+
+// For each pokemon element, add an on click listener to play the corresponding sound
+pokemons.forEach((pokemon, index) => {
+    pokemon.addEventListener("click", event => {
+        var audio = new Audio(sounds[index]);
+        audio.play();
+    })
+})
