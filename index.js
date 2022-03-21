ffe94c115411c4abc57be420289f5958f3a232fb
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2022-03-21 12:07:11 +0100

    v0.1.1
    Compress images.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const sounds = ["https://pokemoncries.com/cries-old/1.mp3", "https://pokemoncrie
 // Get all elements with class pokemon
 const pokemons = document.querySelectorAll(".pokemon");
 
-// For each pokemon element, add an on click listener to play the corresponding sound
+// For each pokemon element, add an on-click listener to play the corresponding sound
 pokemons.forEach((pokemon, index) => {
     pokemon.addEventListener("click", event => {
         var audio = new Audio(sounds[index]);
